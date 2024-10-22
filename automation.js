2a8f1b84a98c8522c5bab2cabd3bbbd57701e182
Umar Farook <umarkhaji456@gmail.com>
2024-10-22 18:37:15 +0530

    add inquirer prompt

--- automation.js
+++ automation.js
@@ -1,5 +1,40 @@
-const startAutomation = () => {
-  console.log('automation started...')
-}
+const inquirer = require("inquirer");
 
-startAutomation()
+const getInputs = async () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'resultLink',
+      message: 'Enter the link to result page:',
+      default: 'https://sbtet.ap.gov.in/APSBTET/results.do',
+    },
+    {
+      type: 'list',
+      name: 'semester',
+      message: 'Which semester do you want to check results for?',
+      choices: ['1YEAR', '3SEM', '4SEM', '5SEM', '6SEM'],
+      default: '4SEM',
+    },
+    {
+      type: 'input',
+      name: 'admissionYear',
+      message: 'What is your admission year?',
+      default: '22',
+    },
+    {
+      type: 'list',
+      name: 'branch',
+      message: 'Which branch are you following?',
+      choices: ['CM', 'EC', 'M', 'EE'],
+      default: 'CM',
+    },
+  ]);
+};
+
+const startAutomation = async () => {
+  console.log('automation started...');
+  const inputs = await getInputs();
+  console.log(inputs)
+};
+
+startAutomation();
